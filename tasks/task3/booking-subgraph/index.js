b7c79315c0dc901a86a71faf8b8c5c1bc56ac3e6
Anton Bessolitsyn <antonbessolitsyn@yandex.ru>
2025-08-14 16:26:50 +0300

    modified:   tasks/task3/booking-subgraph/index.js

--- tasks/task3/booking-subgraph/index.js
+++ tasks/task3/booking-subgraph/index.js
@@ -37,7 +37,7 @@ const resolvers = {
       var req = { user_id: userId };
       var bookings = await bookingService.listBookings(req)
       console.log(bookings);
-      return r.bookings
+      return bookings
     }
   },
   Booking:{
