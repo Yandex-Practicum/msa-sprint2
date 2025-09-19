1fa9333867beabea4e9d13115c3e2e633df9c619
Виталий Кожанов <vvkozhanov@domclick.ru>
2025-09-19 10:50:19 +0500

    sprint 2

--- hotelio-monolith/src/main/java/com/hotelio/monolith/repository/PromoCodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hotelio.monolith.repository;
-
-import com.hotelio.monolith.entity.PromoCode;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PromoCodeRepository extends JpaRepository<PromoCode, String> {
-}
