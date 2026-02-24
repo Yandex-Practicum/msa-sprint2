653ae340595cd52b11fd16a35c233553ae76b8e1
rof1ll <dimabelyaev.cit@gmail.com>
2026-02-24 20:06:00 +0300

    Add task2

--- tasks/task2/booking-history-service/src/migrations.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists booking_history (
-  id bigserial primary key,
-  event_id text unique not null,
-  event_type text not null,
-  occurred_at timestamptz not null,
-  booking_id text not null,
-  payload jsonb not null
-);
