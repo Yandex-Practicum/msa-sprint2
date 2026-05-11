365b85be7667a73f3275fa386bd45eac1169f889
natabelousova <NataBelousova@x5.ru>
2026-05-11 14:26:48 +0300

    Tasks 3

--- tasks/task3/gateway/index.js
+++ tasks/task3/gateway/index.js
@@ -1,20 +1,32 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { ApolloGateway } from '@apollo/gateway';
+import { ApolloGateway, RemoteGraphQLDataSource } from '@apollo/gateway';
 
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+  willSendRequest({ request, context }) {
+    if (context.headers) {
+      Object.entries(context.headers).forEach(([key, value]) => {
+        request.http.headers.set(key, value);
+      });
+    }
+  }
+}
 
 const gateway = new ApolloGateway({
   serviceList: [
     { name: 'booking', url: 'http://booking-subgraph:4001' },
     { name: 'hotel', url: 'http://hotel-subgraph:4002' }
-  ]
+  ],
+  buildService({ name, url }) {
+    return new AuthenticatedDataSource({ url });
+  }
 });
 
-const server = new ApolloServer({ gateway, subscriptions: false });
+const server = new ApolloServer({ gateway });
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
-  context: async ({ req }) => ({ req }), // headers пробрасываются
+  context: async ({ req }) => ({ headers: req.headers })
 }).then(({ url }) => {
   console.log(`🚀 Gateway ready at ${url}`);
 });
